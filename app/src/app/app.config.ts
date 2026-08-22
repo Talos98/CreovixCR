5c4840a96148d0958fc3ffa97297bf6d10eeed0a
Usuario <Usuario@DESKTOP-LEF24UE>
2026-08-22 16:07:44 -0600

    Adjust cita detail
    Corrección de fecha que aparecía en inglés y espacios de la información

--- app/src/app/app.config.ts
+++ app/src/app/app.config.ts
@@ -1,4 +1,6 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
 import { provideCalendar, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { provideRouter } from '@angular/router';
@@ -8,8 +10,11 @@ import { routes } from './app.routes';
 import { httpErrorInterceptor } from './core/interceptors/http-error.interceptor';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 
+registerLocaleData(localeEs);
+
 export const appConfig: ApplicationConfig = {
   providers: [
+    { provide: LOCALE_ID, useValue: 'es' },
     provideCalendar({
       provide: DateAdapter,
       useFactory: adapterFactory,
